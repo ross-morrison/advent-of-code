2c07f776218fbfd9b0d83df894394204a7ff02f6
Ross <45754264+ross-morrison@users.noreply.github.com>
2022-11-30 21:28:14 -0800

    Advent Day 1

--- 2022/1/a.ts
+++ 2022/1/a.ts
@@ -1,4 +1,10 @@
 const input = Deno.readTextFileSync("2022/1/input-a.txt");
-console.log(input.split(""));
+
+const totals = input.split("\n\n").map((elf) => {
+    return elf.split("\n").reduce((acc, val) => acc + parseInt(val), 0);
+}).sort((a: number, b: number) => b - a);
+
+const answer = totals[0];
+console.log(answer);
 
 Deno.exit(0);
---
+++ 2022/1/b.ts
@@ -0,0 +1,10 @@
+const input = Deno.readTextFileSync("2022/1/input-a.txt");
+
+const totals = input.split("\n\n").map((elf) => {
+    return elf.split("\n").reduce((acc, val) => acc + parseInt(val), 0);
+}).sort((a: number, b: number) => b - a);
+
+const answer = totals[0] + totals[1] + totals[2];
+console.log(answer);
+
+Deno.exit(0);
