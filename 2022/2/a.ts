2062a649a2242676f7c752af3bd71aab215d6d44
Ross <ross.morrison312@gmail.com>
2022-12-01 21:19:47 -0800

    Day 2

--- 2022/2/a.ts
+++ 2022/2/a.ts
@@ -1,3 +1,53 @@
 const input = Deno.readTextFileSync("input-a.txt");
 const lines = input.split("\n");
-const grouped = input.split("\n\n");
+
+const parsedAsColumns = lines.reduce((acc: any, line) => {
+  const cols = line.split(" ");
+  for (let i = 0; i < cols.length; i++) {
+    if (!acc[i]) {
+      acc[i] = [];
+    }
+    acc[i].push(cols[i]);
+  }
+  return acc;
+}, []);
+
+const values: any = { rock: 1, paper: 2, scissors: 3 };
+
+const playerInput: any = {
+  A: "rock",
+  B: "paper",
+  C: "scissors",
+  X: "rock",
+  Y: "paper",
+  Z: "scissors",
+};
+
+let score = 0;
+for (let i = 0; i < lines.length; i++) {
+  const player1 = parsedAsColumns[0][i];
+  const player2 = parsedAsColumns[1][i];
+
+  const parsed1 = playerInput[player1];
+  const parsed2 = playerInput[player2];
+
+  if (parsed1 === parsed2) {
+    score += 3;
+  }
+
+  if (parsed1 === "rock" && parsed2 === "paper") {
+    score += 6;
+  }
+
+  if (parsed1 === "paper" && parsed2 === "scissors") {
+    score += 6;
+  }
+
+  if (parsed1 === "scissors" && parsed2 === "rock") {
+    score += 6;
+  }
+
+  score += values[parsed2];
+}
+
+console.log(score);
--- 2022/2/b.ts
+++ 2022/2/b.ts
@@ -1,3 +1,72 @@
 const input = Deno.readTextFileSync("input-a.txt");
 const lines = input.split("\n");
-const grouped = input.split("\n\n");
+
+const parsedAsColumns = lines.reduce((acc: any, line) => {
+  const cols = line.split(" ");
+  for (let i = 0; i < cols.length; i++) {
+    if (!acc[i]) {
+      acc[i] = [];
+    }
+    acc[i].push(cols[i]);
+  }
+  return acc;
+}, []);
+
+const playerInput: any = {
+  A: "rock",
+  B: "paper",
+  C: "scissors",
+  X: "lose",
+  Y: "draw",
+  Z: "win",
+};
+
+let score = 0;
+for (let i = 0; i < lines.length; i++) {
+  const player1 = parsedAsColumns[0][i];
+  const player2 = parsedAsColumns[1][i];
+
+  const parsed1 = playerInput[player1];
+  const parsed2 = playerInput[player2];
+
+  if (parsed2 === "draw") {
+    score += 3;
+    if (parsed1 === "rock") {
+      score += 1;
+    }
+    if (parsed1 === "paper") {
+      score += 2;
+    }
+    if (parsed1 === "scissors") {
+      score += 3;
+    }
+  }
+
+  if (parsed2 === "win") {
+    score += 6;
+    if (parsed1 === "rock") {
+      score += 2;
+    }
+    if (parsed1 === "paper") {
+      score += 3;
+    }
+    if (parsed1 === "scissors") {
+      score += 1;
+    }
+  }
+
+  if (parsed2 === "lose") {
+    score += 0;
+    if (parsed1 === "scissors") {
+      score += 2;
+    }
+    if (parsed1 === "rock") {
+      score += 3;
+    }
+    if (parsed1 === "paper") {
+      score += 1;
+    }
+  }
+}
+
+console.log(score);
