af893d807ed24629cac38c36f56f53b25c1870aa
Ross <ross.morrison312@gmail.com>
2022-12-02 21:49:51 -0800

    Day 3

--- 2022/3/a.ts
+++ 2022/3/a.ts
@@ -1,3 +1,30 @@
 const input = Deno.readTextFileSync("input-a.txt");
+const lines = input.split("\n");
 
-const grouped = input.split(",");
+const items = lines.map((line) => line.trim().split(""));
+
+const split = items.reduce<any>((acc, val, index) => {
+  if (!acc[index]) {
+    acc[index] = [];
+  }
+  acc[index][0] = val.slice(0, val.length / 2);
+  acc[index][1] = val.slice(val.length / 2);
+  return acc;
+}, []);
+
+const getPriority = (item: string) => {
+  return item.charCodeAt(0) > 97 ? item.charCodeAt(0) - 96 : item.charCodeAt(0) - 38;
+};
+
+const sum = split.reduce((acc: number, val: string) => {
+  const [left, right] = val;
+  const matching: string[] = [];
+  for (const value of left) {
+    if (right.includes(value) && !matching.includes(value)) {
+        matching.push(value);
+    }
+  }
+  return acc + matching.reduce((acc, val) => acc + getPriority(val), 0);
+}, 0);
+
+console.log(sum);
--- 2022/3/b.ts
+++ 2022/3/b.ts
@@ -1,1 +1,28 @@
 const input = Deno.readTextFileSync("input-a.txt");
+const lines = input.split("\n");
+
+const items = lines.map((line) => line.trim().split(""));
+
+const getPriority = (item: string) => {
+  return item.charCodeAt(0) > 97
+    ? item.charCodeAt(0) - 96
+    : item.charCodeAt(0) - 38;
+};
+
+const groupsOfThree = [];
+for (let i = 0; i < items.length; i += 3) {
+  groupsOfThree.push(items.slice(i, i + 3));
+}
+
+const sum = groupsOfThree.reduce<any>((acc, val) => {
+  const [left, right, middle] = val;
+  const matching: string[] = [];
+  for (const value of left) {
+    if (right.includes(value) && middle.includes(value) && !matching.includes(value)) {
+      matching.push(value);
+    }
+  }
+  return acc + matching.reduce((acc, val) => acc + getPriority(val), 0);
+}, 0);
+
+console.log(sum);
